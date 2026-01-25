2e3a9d34a07de8b56e460f7e9f0e44d1a9c64adb
shopeasy6994 <shopeasy.bte@gmail.com>
2026-01-25 11:49:41 +0530

    local server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.ordereasy.win/api/';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
