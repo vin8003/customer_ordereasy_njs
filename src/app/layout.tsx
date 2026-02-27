7b87f86f70bd51e04a5350946af8c74a2b4d9391
shopeasy6994 <shopeasy.bte@gmail.com>
2026-02-27 22:06:58 +0530

    global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import BottomNav from "@/app/components/BottomNav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -29,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased pb-48 overflow-x-hidden`}>
+      <body className={`${inter.variable} font-sans antialiased pb-48 overflow-x-hidden`}>
         <CartProvider>
           <NotificationProvider>
             <NotificationWrapper>
